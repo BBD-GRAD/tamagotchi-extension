f5b154254c306fecb14b5130c236589281083c95
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2024-04-07 10:06:51 +0200

    fixed flyway workflow

---
+++ tamagotchi-database/scripts/R__test_script.sql
@@ -0,0 +1,18 @@
+-- Drop the table if it exists
+IF OBJECT_ID('example_table', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE example_table;
+END
+GO
+
+-- Create the table
+CREATE TABLE example_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(100)
+);
+GO
+
+-- Insert data into the table
+INSERT INTO example_table (id, name) VALUES (1, 'Example 1');
+INSERT INTO example_table (id, name) VALUES (2, 'Example 2_v2');
+GO
