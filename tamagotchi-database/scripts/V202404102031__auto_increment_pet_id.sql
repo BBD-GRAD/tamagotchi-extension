67de9cb232cd524543a2861f79b6cae05224cda2
arinaho-muleba <157449847+arinaho-muleba@users.noreply.github.com>
2024-04-10 21:24:00 +0200

    auto incrementing id on Pets

--- tamagotchi-database/scripts/V202404102031__auto_increment_pet_id.sql
+++ tamagotchi-database/scripts/V202404102031__auto_increment_pet_id.sql
@@ -1,2 +1,14 @@
-ALTER TABLE dbo.Pets
-    ALTER COLUMN pet_id INT IDENTITY(1,1);
+DROP TABLE dbo.Pets;
+
+CREATE TABLE dbo.Pets (
+    pet_id INT IDENTITY(1,1) NOT NULL,
+    user_id VARCHAR(50) NOT NULL,
+    pet_name VARCHAR(50) NOT NULL,
+    xp BIGINT NOT NULL,
+    health FLOAT NOT NULL,
+    food FLOAT NOT NULL,
+    water FLOAT NOT NULL,
+    stamina FLOAT NOT NULL,
+    CONSTRAINT pets_pet_id_primary PRIMARY KEY (pet_id),
+    CONSTRAINT pets_user_id_foreign FOREIGN KEY (user_id) REFERENCES dbo.Users (user_id)
+);
